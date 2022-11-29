12bf8b2495584d28ceafdd803b410c93c90489e1
Tadej Mohorcic <tadej.mohorcic@gmail.com>
2022-11-29 15:19:35 +0100

    dodan opis

--- Kviz 1/simpson2D.m
+++ Kviz 1/simpson2D.m
@@ -1,19 +1,29 @@
 function val = simpson2D(f,a,b,c,d,n,m)
+% function val = simpson2D(f,a,b,c,d,n,m)
+%
+% Sestavljeno Simpsonovo pravilo za računanje integralov dveh spremenljivk.
+%
+% Vhodni podatki
+% funkcija f spremenljivk x in y,
+% intervala [a,b] in [c,d],
+% število delilnih točk n in m intervalov [a,b] in [c,d].
+%
+% Izhodni podatek
+% približek za integral.
+
 h1 = (b-a)/n;
 h2 = (d-c)/m;
 
 v = zeros(1,n+1);
 u = zeros(1,m+1);
+val = 0;
 
 for i = 1:n+1
     v(i) = a + (i-1)*h1;
 end
-
 for i = 1:m+1
     u(i) = c + (i-1)*h2;
 end
-
-val = 0;
 
 for i = 1:n
     for j = 1:m
@@ -24,12 +34,22 @@ end
 end
 
 function val = simpson2Dh(f,a,b,c,d)
+% function val = simpson2Dh(f,a,b,c,d)
+%
+% Pomožna funkcija, ki izračuna integral po Simpsonovem pravilu.
+%
+% Vhodni podatki
+% funkcija f spremenljivk x in y,
+% intervala [a,b] in [c,d].
+%
+% Izhodni podatek
+% približek za integral.
+
 e = (b-a)/2;
 d = (d-c)/2;
 
 v = [1 4 1];
 M = v'.*v;
-
 val = 0;
 
 for i = 1:3
--- Kviz 1/trapezoid2D.m
+++ Kviz 1/trapezoid2D.m
@@ -1,21 +1,31 @@
 function val = trapezoid2D(f,a,b,c,d,n,m)
+% function val = trapezoid2D(f,a,b,c,d,n,m)
+%
+% Sestavljeno trapezno pravilo za računanje integralov dveh spremenljivk.
+%
+% Vhodni podatki
+% funkcija f spremenljivk x in y,
+% intervala [a,b] in [c,d],
+% število delilnih točk n in m intervalov [a,b] in [c,d].
+%
+% Izhodni podatek
+% približek za integral.
+
 h1 = (b-a)/n;
 h2 = (d-c)/m;
 
 v = ones(1,n+1);
 u = ones(1,m+1);
+val = 0;
 
 for i = 2:n
     v(i) = 2;
 end
-
 for i = 2:m
     u(i) = 2;
 end
 
 M = v'.*u;
-
-val = 0;
 
 for i = 1:n+1
     for j = 1:m+1
